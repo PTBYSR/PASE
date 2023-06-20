284953c865c5267ed6a27225512dd71eb90d792c
PTBYSR <paul.emechebe@gmail.com>
2023-06-20 19:21:08 +0100

    huh

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,4 +1,5 @@
 
+import NavBox from "components/interface/NavBox/NavBox";
 import Footer from "components/layouts/Footer/Footer";
 import type { ReactNode } from "react";
 import React from "react";
@@ -10,7 +11,9 @@ type IMainProps = {
 
 const Main = (props: IMainProps) => (
   <div className="inter bg-midnight">
-      
+      <div className="md:hidden block px-6 md:px-14 pt-10 mb-10">
+        <NavBox />
+      </div>
       {props.meta}
         <main className="bg-midnight">{props.children}</main>
         <Footer />
